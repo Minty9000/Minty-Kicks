4e7510f3c62185f756d7686af2a07651c80210b4
Minty9000 <gilbertoteng86@gmail.com>
2024-07-27 00:45:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,21 @@ app.post('/data', (req, res) => {
     res.status(201).json(product);
 });
 
+const url = `https://final-test-7fvk.onrender.com`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 // Route to get all products
 app.get('/data', (req, res) => {
     res.json(products);
